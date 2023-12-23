10201a0455c4904ed32e670c011dde4702657a9a
Tom <qytang2014@126.com>
2023-12-23 23:24:31 +0800

    update gtags config that can support gtags scope

--- lua/cscope/init.lua
+++ lua/cscope/init.lua
@@ -4,13 +4,13 @@ local log = require("utils.log")
 local M = {}
 
 M.opts = {
-	db_file = "./cscope.out",
-	exec = "cscope",
+	-- db_file = "./cscope.out",
+	db_file = "./GTAGS",
+	exec = "gtags-cscope",
 	picker = "quickfix",
 	qf_window_size = 5,
 	qf_window_pos = "bottom",
 	skip_picker_for_single_result = false,
-	db_build_cmd_args = { "-bqkv" },
 	statusline_indicator = nil,
 }
 
@@ -115,7 +115,7 @@ end
 local cscope_find_helper = function(op_n, op_s, symbol, hide_log)
 	-- Executes cscope search and shows result in QuickFix List or Telescope
 
-	local db_file = vim.g.cscope_maps_db_file or M.opts.db_file
+	local db_file = (vim.b.gutentags_files ~= nil and vim.b.gutentags_files["cscope_maps"]) or M.opts.db_file
 	local cmd = M.opts.exec
 
 	if cmd == "cscope" then
@@ -125,7 +125,7 @@ local cscope_find_helper = function(op_n, op_s, symbol, hide_log)
 			log.warn("'d' operation is not available for " .. M.opts.exec, hide_log)
 			return RC.INVALID_OP
 		end
-		db_file = "GTAGS" -- This is only used to verify whether db is created or not.
+		-- db_file = "GTAGS" -- This is only used to verify whether db is created or not.
 	else
 		log.warn("'" .. cmd .. "' executable is not supported", hide_log)
 		return RC.INVALID_EXEC
@@ -216,10 +216,24 @@ local function cscope_build_output(err, data)
 	end
 end
 
+--获取路径
+local function getpath(filename)
+	return filename:match("(.*[/\\])")
+end
+
+local function script_path()
+	local str = debug.getinfo(1, "S").source:sub(2)
+	return str:match("(.*[/\\])") --删除后面的文件，只保留路径
+end
+
 local cscope_build = function()
 	local stdout = vim.loop.new_pipe(false)
 	local stderr = vim.loop.new_pipe(false)
-	local db_build_cmd_args = M.opts.db_build_cmd_args
+	local db_build_cmd_args = {}
+	local db_file = (vim.b.gutentags_files ~= nil and vim.b.gutentags_files["cscope_maps"]) or M.opts.db_file
+	local proj_root = vim.b.gutentags_root or "./"
+	local file_list_cmd = vim.g.gutentags_file_list_command
+	local opts_exec = nil
 
 	if vim.g.cscope_maps_statusline_indicator then
 		log.warn("db build is already in progress")
@@ -227,14 +241,44 @@ local cscope_build = function()
 	end
 
 	if M.opts.exec == "cscope" then
+		if not (vim.fn.has("win32") or vim.fn.has("win64")) then
+			opts_exec = script_path() .. "plat/win32/update_scopedb.cmd"
+		else
+			opts_exec = script_path() .. "plat/unix/update_scopedb.sh" --unix system
+		end
+		table.insert(db_build_cmd_args, "-p")
+		table.insert(db_build_cmd_args, proj_root)
 		table.insert(db_build_cmd_args, "-f")
-		table.insert(db_build_cmd_args, M.opts.db_file)
+		table.insert(db_build_cmd_args, db_file)
+		if vim.g.gutentags_cscope_build_inverted_index_maps ~= nil then
+			table.insert(db_build_cmd_args, "-I")
+		end
+	elseif M.opts.exec == "gtags-cscope" then
+		--lua中0为true
+		if not (vim.fn.has("win32") or vim.fn.has("win64")) then
+			opts_exec = script_path() .. "plat/win32/update_gtags.cmd"
+		else
+			opts_exec = script_path() .. "plat/unix/update_gtags.sh" --unix system
+		end
+		table.insert(db_build_cmd_args, "--incremental")
+		table.insert(db_build_cmd_args, getpath(db_file))
 	end
+	if file_list_cmd ~= nil then
+		table.insert(db_build_cmd_args, "-L")
+		table.insert(db_build_cmd_args, file_list_cmd)
+	end
+	-- log.warn("opt_exec: " .. opts_exec)
+	-- log.warn("proj_root: " .. proj_root)
+	-- log.warn("db_file: " .. db_file)
+
+	-- for k,v in pairs(db_build_cmd_args) do
+	-- 	log.warn("content k: " .. k .. ", v: " .. v)
+	-- end;
 
 	local handle = nil
-	vim.g.cscope_maps_statusline_indicator = M.opts.statusline_indicator or M.opts.exec
+	vim.g.cscope_maps_statusline_indicator = M.opts.statusline_indicator or opts_exec
 	handle = vim.loop.spawn(
-		M.opts.exec,
+		opts_exec,
 		{
 			args = db_build_cmd_args,
 			stdio = { nil, stdout, stderr },
